25e08cfd62cde2fdce2afa4f8155ce172138d01b
Jonathan Havstad <jonathan.havstad@outlook.com>
2021-08-01 23:32:14 -0700

    Apply fix to only return PRs that are open.

--- app/src/main/java/com/jjhavstad/githubprdiffviewer/util/PrResponseParser.kt
+++ app/src/main/java/com/jjhavstad/githubprdiffviewer/util/PrResponseParser.kt
@@ -13,14 +13,17 @@ class PrResponseParser : ApiResponseParser<String, List<Pr>> {
                 val title = prObject.getString(PR_TITLE_KEY)
                 val url = prObject.getString(PR_URL_KEY)
                 val diffUrl = prObject.getString(PR_DIFF_URL_KEY)
-                _prList.add(
-                    Pr(
-                        id = id,
-                        title = title,
-                        url = url,
-                        diffUrl = diffUrl
+                val state = prObject.getString(PR_STATE_KEY)
+                if (state == PR_STATE_OPEN_VALUE) {
+                    _prList.add(
+                        Pr(
+                            id = id,
+                            title = title,
+                            url = url,
+                            diffUrl = diffUrl
+                        )
                     )
-                )
+                }
             }
         }
     }
@@ -30,5 +33,7 @@ class PrResponseParser : ApiResponseParser<String, List<Pr>> {
         private val PR_TITLE_KEY = "title"
         private val PR_URL_KEY = "url"
         private val PR_DIFF_URL_KEY = "diff_url"
+        private val PR_STATE_KEY = "state"
+        private val PR_STATE_OPEN_VALUE = "open"
     }
 }
